0653f722b5ca55a033486ac741f7449cbf876b9e
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-11-25 11:48:11 -0800

    livecode

--- src/main/java/io/github/auberonedu/equalsLivecode/Centroid.java
+++ src/main/java/io/github/auberonedu/equalsLivecode/Centroid.java
@@ -22,4 +22,66 @@ public class Centroid {
     public String getName() {
         return name;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + r;
+        result = prime * result + c;
+        // result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Centroid other = (Centroid) obj;
+        if (r != other.r)
+            return false;
+        if (c != other.c)
+            return false;
+        // if (name == null) {
+        //     if (other.name != null)
+        //         return false;
+        // } else if (!name.equals(other.name))
+        //     return false;
+        return true;
+    }
+
+    
+    // // this is not needed throughout the method, java assumes you are talking about this when referenceing any instance variable
+    // @Override
+    // public boolean equals(Object o) {
+    //     // is this equal to o
+    //     // ensure r, c, and name are all equal between this and o
+    //     // ensure we meet Java equals contract
+
+    //     // check if o is null
+    //     if (o == null) return false;
+
+    //     // if classes are not equal return false
+    //     if(!this.getClass().equals(o.getClass())) return false;
+
+    //     // cast Centroid type
+    //     Centroid other = (Centroid) o;
+
+    //     // check fields
+    //     if (this.r != other.r) return false;
+    //     if (this.c != other.c) return false;
+
+    //     // check if .name is null
+    //     if (this.name == null) {
+    //         // if (other.name == null) return true;
+    //         // return false;
+    //         return other.name == null;
+    //     }
+
+    //     return this.name.equals(other.name);
+    // }
 }
