526df907adcdce7dce5033fa4ee1a7b3c2a0da45
rjfredr <fredrickalexander1727@gmail.com>
2025-11-25 23:39:22 -0800

    first part overrides

--- src/main/java/io/github/auberonedu/equalsLivecode/Centroid.java
+++ src/main/java/io/github/auberonedu/equalsLivecode/Centroid.java
@@ -22,4 +22,46 @@ public class Centroid {
     public String getName() {
         return name;
     }
+
+    public int HashCode() {
+        final int prime = 25;
+        int result = 1;
+        result = prime+result+c;
+        result = prime+result+r;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this==obj) return true;
+        if (obj==null) return false;
+        if (getClass()!=obj.getClass()) return false;
+        Centroid o = (Centroid) obj;
+        if (r!= o.r) return false;
+        if (c!= o.c) return false;
+        return true;
+    }
+
+    // @Override
+    // public boolean equals(Object o) {
+    //     if (o==null) return false;
+    //     if (!this.getClass().equals(o.getClass())) {
+    //         return false;
+    //     }
+
+    //     Centroid other = (Centroid) o;
+    //     if (this.r != other.r) return false;
+    //     if (this.c != other.c) return false;
+
+    //     if (this.name==null) {
+    //         if (other.name==null) {
+    //             return true;
+    //         }
+    //         else {
+    //             return false;
+    //         }
+    //     }
+    //     if (!this.name.equals(other.name)) return false;
+    //     return true;
+    // }
 }
